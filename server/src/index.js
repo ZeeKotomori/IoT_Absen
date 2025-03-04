38330b18c239131aad22c9725b7d9c0960d3540b
ZeeKotomori <zikriaf23@gmail.com>
2025-03-04 23:40:42 +0700

    add teacher routes to main application for handling teacher-related requests

--- server/src/index.js
+++ server/src/index.js
@@ -1,18 +1,15 @@
 import express from "express";
-import mqtt from "mqtt";
-import { PrismaClient } from "@prisma/client";
-import bycrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
 import client from "./service/mqttHandler.js"; 
 import cookie from "cookie-parser"
 import authRoute from "./routes/authRoute.js";
+import teacherRoute from "./routes/teacherRoute.js";
 
 const app = express();
-const prisma = new PrismaClient();
 
 app.use(cookie());
 app.use(express.json());
 app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/teacher", teacherRoute);
 
 app.listen(3000, () => {
     console.log('API running on http://localhost:3000');
