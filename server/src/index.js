f727c8b6035a83086f4f04ccef12a257f5a71bac
ZeeKotomori <zikriaf23@gmail.com>
2025-03-04 19:31:28 +0700

    refactor server setup to integrate authentication routes and improve logging

--- server/src/index.js
+++ server/src/index.js
@@ -1,20 +1,19 @@
 import express from "express";
+import mqtt from "mqtt";
 import { PrismaClient } from "@prisma/client";
-import { absensiQueue } from "./queueWorker.js";
-import "./mqttHandler.js";
+import bycrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import client from "./service/mqttHandler.js"; 
+import cookie from "cookie-parser"
+import authRoute from "./routes/authRoute.js";
 
 const app = express();
 const prisma = new PrismaClient();
 
+app.use(cookie());
 app.use(express.json());
+app.use("/api/v1/auth", authRoute);
 
-app.get("/", (req, res) => {
-    res.send("API Absensi IoT Running!");
+app.listen(3000, () => {
+    console.log('API running on http://localhost:3000');
 });
-
-app.get("/api/v1/absensi", async (req, res) => {
-    const data = await prisma.absensi.findMany();
-    res.json(data);
-});
-
-app.listen(3000, () => console.log("🚀 Backend running on port 3000"));
