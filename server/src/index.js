e4fe6e0f5b977e2d60fc4ec854fa5627b2de9246
ZeeKotomori <zikriaf23@gmail.com>
2025-03-08 23:27:16 +0700

    feat: integrate attendance route and schedule jobs in the main application

--- server/src/index.js
+++ server/src/index.js
@@ -1,17 +1,21 @@
 import express from "express";
-import client from "./service/mqttHandler.js";
+import client from "./service/mqtt/mqttHandler.js";
 import cookie from "cookie-parser";
 import authRoute from "./routes/authRoute.js";
 import subjectTeacherRoute from "./routes/subjectTeacherRoute.js";
+import routerAttendance from "./routes/attendanceRoute.js";
+import { scheduleJobs } from "./service/queueService.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookie());
 
+app.use("/api/v1/", routerAttendance);
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/teacher", subjectTeacherRoute);
 
 app.listen(3000, () => {
     console.log("API running on http://localhost:3000");
+    scheduleJobs();
 });
