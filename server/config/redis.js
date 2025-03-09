7519c3fe69172986e5777fc1965e0e5a2f31b772
ZeeKotomori <zikriaf23@gmail.com>
2025-03-10 00:52:08 +0700

    feat: configure Redis client to allow unlimited retries per request

--- server/config/redis.js
+++ server/config/redis.js
@@ -3,6 +3,7 @@ import IORedis from "ioredis";
 export const redis = new IORedis({
     host: "127.0.0.1",
     port: 6379,
+    maxRetriesPerRequest: null
 });
 
 redis.on("error", (err) => console.error("❌ Redis Error:", err));
